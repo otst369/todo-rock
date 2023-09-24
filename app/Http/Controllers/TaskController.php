49c4f9dba2799b0d208b443456e7879473cfab64
mhayashi91 <awesome_box@icloud.com>
2023-09-25 04:01:52 +0800

    TaskController createまで
    一旦寝ます

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,12 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Task; 
 
 class TaskController extends Controller
 {
     //
     function index()
     {
-        return view('tasks.index');
+        $tasks = Task::all();
+        return view('tasks.index', compact('tasks'));
     }
+
+    function create(Request $request)
+    {
+        $validator = $request->validate([
+            'title' => ['required', 'string','max:30' ],
+            'contents' =>['required', 'string','max:140'],
+            'image_at' =>['required', 'image']
+        ]);
+        return view('tasks.create');
+    }
+
+
 }
