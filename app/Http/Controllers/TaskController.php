74837a51d3f1ea21e4fd717200b36a0bfeea23f7
mhayashi91 <awesome_box@icloud.com>
2023-09-25 09:41:25 +0800

    TaskControllerにコメントつけた
    TaskControllerにコメントつけた。showをコメントアウトした。

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -9,13 +9,14 @@ use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
-    //
+    //一覧表示
     function index()
     {
         $tasks = Task::all();
         return view('tasks.index', compact('tasks'));
     }
 
+    //新規作成
     function create(Request $request)
     {
         $validator = $request->validate([
@@ -26,6 +27,7 @@ class TaskController extends Controller
         return view('tasks.create');
     }
 
+    //新規投稿保存
     function store(Request $request)
     {
         $task = new Task;
@@ -38,18 +40,21 @@ class TaskController extends Controller
         return redirect()->route('tasks.index');
     }
 
-    function show($id)
-    {
-        $task = Task::find($id);
-        return view('tasks.show', compact('task'));
-    }
+    // function show($id)
+    // {
+    //     $task = Task::find($id);
+    //     return view('tasks.show', compact('task'));
+    // }
 
+
+    //編集
     function edit($id)
     {
         $task = Task::find($id);
         return view('tasks.edit',['task' => $task]);
     }
 
+    //編集したデータを保存
     function update(Request $request, $id)
     {
         $task = Task::find($id);
@@ -64,6 +69,7 @@ class TaskController extends Controller
         return redirect()->route('tasks.show', ['id' => $id]);
     }
 
+    //投稿削除
     function destroy($id)
     {
         $task = Task::find($id);
