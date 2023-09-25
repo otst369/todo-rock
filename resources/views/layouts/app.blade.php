ad1c3151a561b4e7c20b29c5db41001256e67b96
mhayashi91 <awesome_box@icloud.com>
2023-09-25 11:43:03 +0800

    消えたのでcreateとcontroller編集した
    消えたのでcreateとcontroller編集した

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,9 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
 </head>
 <body>
     <div id="app">
