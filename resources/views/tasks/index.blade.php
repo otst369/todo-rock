dcfb162abe8800d52b07245f20ad050ce2d850b8
otst <121769026+otst369@users.noreply.github.com>
2023-09-28 01:11:06 +0800

    禁断

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.app')
+@section('content')
+
+    <head>
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    </head>
+
+
+    <header>
+        <div class="header-left">
+            <span class="todo-text">検索機能</span>
+            <form action="{{ route('tasks.search') }}" method="GET">
+                <input type="text" name="query" placeholder="検索キーワード">
+                <button type="submit">検索</button>
+            </form>
+            {{-- <div class="search-bar">
+      <input type="text" id="searchInput" placeholder="検索したい用語を入力">
+      <button id="searchButton" class="search-button">Go</button>
+    </div> --}}
+        </div>
+        <div class="user-info">
+            <button class="register-button">
+                <a href="{{ route('tasks.create') }}">新規投稿</a>
+            </button>
+
+        </div>
+    </header>
+
+    <div class="big-container">
+        @foreach ($tasks as $task)
+            <div class="post-box">
+                <h2 class="name">{{ $task->user->name }}</h2>
+                <div class="image-container">
+                    <img src="{{ url('storage/images/' . $task->image_at) }}" alt="Image">
+                </div>
+                <div class="post-content">
+                    <h2 class="title">{{ $task->title }}</h2>
+                    <p class="contents">{{ $task->contents }}</p>
+
+                    <div class="inside-container">
+                        @if($task->user_id == Auth::user()->id)
+                        <div class="bookmark">
+                          <a href="#"><i class="far fa-bookmark"></i></a>
+                        </div> 
+                        <a href="{{ route('tasks.edit', $task->id) }}" class="edit-button">編集</a>
+                        <form action='{{ route('tasks.destroy', $task->id) }}' method='post'>
+                            @csrf
+                            @method('delete')
+
+                            <input type='submit' value='削除'class="btn btn-danger"
+                                onclick='return confirm("本当に削除しますか？");'>
+                            {{-- <button class="delete-button">削除</button> --}}
+                        </form>
+                        {{-- <a href="{{ route('tasks.show', $task->id) }}">View Details</a> --}}
+                        @endif
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endsection
