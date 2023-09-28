ee8c6ed637c441d283181918b85e437c723c2470
mhayashi91 <awesome_box@icloud.com>
2023-09-28 12:59:26 +0800

    完璧かもしれん

--- database/migrations/2023_09_22_033409_create_comments_table.php
+++ database/migrations/2023_09_22_033409_create_comments_table.php
@@ -16,10 +16,10 @@ class CreateCommentsTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->text('body');
-            $table->bigInteger('task_id');
-            $table->bigInteger('user_id');
-            $table->foreign('task_id')->references('id')->on('task');
-            $table->foreign('user_id')->references('id')->on('user');
+            $table->unsignedBigInteger('task_id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('task_id')->references('id')->on('tasks');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
--- resources/views/comments/create.blade.php
+++ resources/views/comments/create.blade.php
@@ -3,28 +3,13 @@
 
 <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
 
-{{-- <div class="container mt-5">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <form action="{{ route('comments.store') }}" method="POST">
-                @csrf
-                <h2>コメント</h2>
-                <div class="form-group">
-                    <textarea class="form-control" placeholder="内容" rows="5" name="body">
-                    </textarea>
-                </div>
-                <button type="submit" class="btn btn-primary">作成</button>
-            </form>
-        </div>
-    </div>
-</div> --}}
 <div class="container">
     <div class="row justify-content-center mt-5">
         <div class="col-md-8">
             <div class="card mt-3">
                 <div class="card-body">
-                    <p class="card-text">内容：{{ $task->contents }}</p>
-                    <p>投稿日時：{{ $task->created_at }}</p>
+                <p class="card-textcreate">内容：{{ $task->title }}</p>
+                <p>投稿日時：{{ $task->created_at }}</p>               
                 </div>
             </div>
         </div>
@@ -33,16 +18,11 @@
         <div class="col-md-8">
             <form action="{{ route('comments.store') }}" method="post">
                 @csrf
-                <div class="form-group">
-                    <label>関連するタスク</label>
-                    <select class="form-control" id="task_id" name="task_id">
-                        <option value="{{ $task->id }}">{{ $task->title }}</option>
-                        <!-- 他のタスクを追加する場合はここにoptionを追加 -->
-                    </select>
-                </div>
+                <input type="hidden" name="task_id" value="{{ $task->id }}">
                 <div class="form-group">
                     <label>コメント</label>
-                    <textarea class="form-control" placeholder="内容" rows="5" name="body"></textarea>
+                    <textarea class="form-control" 
+                    placeholder="内容" rows="5" name="body"></textarea>
                 </div>
                 <button type="submit" class="btn btn-primary mt-3">コメントする</button>
             </form>
