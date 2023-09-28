d8f6dec2f4458f9eaa081809e478d481c524281a
mhayashi91 <awesome_box@icloud.com>
2023-09-28 09:50:54 +0800

    ブックマーク以外のCSSを整えた

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,20 +1,13 @@
 @extends('layouts.app')
 @section('content')
 
-
 <head>
     <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 
-
 <h2>検索結果</h2>
-
 <div class="big-container">
-{{-- @if ($query)
-    <p>検索クエリ: {{ $query }}</p>
-@endif --}}
-
 @if ($tasks->isEmpty())
     <p>該当するタスクはありません。</p>
 @else
@@ -26,18 +19,20 @@
             <div class="post-content">
                 <h2 class="title">{{ $task->title }}</h2>
                 <p class="contents">{{ $task->contents }}</p>
-                <a href="#"><i class="far fa-bookmark"></i></a>
+                <div class="inside-container">
+                    <div class="bookmark">
+                    <a href="#"><i class="far fa-bookmark"></i></a>
+                    </div>
                 <a href="{{ route('tasks.edit', $task->id) }}" class="edit-button">編集</a>
                 <form action='{{ route('tasks.destroy', $task->id) }}' method='post'>
                     @csrf
                     @method('delete')
                     <input type='submit' value='削除' class="btn btn-danger" onclick='return confirm("本当に削除しますか？");'>
                 </form>
-                {{-- <a href="{{ route('tasks.show', $task->id) }}">View Details</a> --}}
+                </div>
             </div>
         </div>
     @endforeach
 @endif
 </div>
-
 @endsection
